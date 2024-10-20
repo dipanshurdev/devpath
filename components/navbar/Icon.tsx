23b14eaf019fcbc89285e342c472c4141fae5ba9
Dipanshu Rawat <dv451197@gmail.com>
2024-10-20 23:59:11 +0530

    add: state and icon component

---
+++ components/navbar/Icon.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import DevPath from "../../assets/icons/path.svg";
+
+type Props = {
+  title: String;
+};
+
+const Icon = ({ title }: Props) => {
+  return (
+    <div className="inline-block border border-dotted px-3 py-2 rounded-lg">
+      <div className="flex flex-row justify-center items-center gap-2">
+        <div className="w-full">
+          <Image
+            src={DevPath}
+            alt="DevPath"
+            width={20}
+            height={20}
+            //   className="stroke-blue-100"
+          />
+        </div>
+        <span className="text-lg font-bold">{title}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Icon;
--- components/roadmaps/RoadmapDiv.tsx
+++ components/roadmaps/RoadmapDiv.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 const RoadmapDiv = ({ id, name }: Props) => {
   return (
-    <div className="px-6 py-3 my-2 bg-neutral-800 rounded-lg flex items-center justify-between ">
-      <span className="text-lg text-blue-100 capitalize">{name}</span>
+    <div className="px-6 py-3 my-2 bg-darkLight rounded-lg flex items-center justify-between ">
+      <span className="text-lg text-primaryWhite capitalize">{name}</span>
       <span className="flex-grow justify-end flex">
         <CiBookmark />
       </span>
