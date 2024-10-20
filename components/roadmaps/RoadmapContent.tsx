22012c3e78bab5673923e8258c397fa3746f1f8d
Dipanshu Rawat <dv451197@gmail.com>
2024-10-20 15:12:39 +0530

    ref: layout and styles

--- components/roadmaps/RoadmapContent.tsx
+++ components/roadmaps/RoadmapContent.tsx
@@ -11,7 +11,7 @@ const RoadmapContent = ({ content, title }: Props) => {
     <div className="flex items-center justify-center w-full flex-col h-full mb-4">
       <div className="text-center w-full py-4 mt-3 mb-6">
         <h3 className="text-2xl font-semibold ">{title}</h3>
-        <div className="h-[0.5px] my-2 w-full bg-blue-100" />
+        <div className="h-[0.5px] my-2 w-full bg-primaryWhite" />
       </div>
 
       <div className="grid grid-cols-4 w-full gap-4 items-center justify-center">
