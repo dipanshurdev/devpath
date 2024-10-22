ff9d8ce983355386487e1b41be836fee9338e94b
Dipanshu Rawat <dv451197@gmail.com>
2024-10-22 23:27:00 +0530

    added functions for signIn, signUp

--- lib/appwrite/api.ts
+++ lib/appwrite/api.ts
@@ -18,3 +18,71 @@
 // ├── .env                    # Environment variables
 // ├── package.json            # Project metadata and dependencies
 // └── next.config.js          # Next.js configuration
+
+import { INewUser } from "@/types";
+import { account, appwriteIds, avatars, databases } from "./config";
+import { ID } from "appwrite";
+
+// CREATE THE USER ACCOUNT
+export async function createAccount(user: INewUser) {
+  try {
+    const createdAccount = await account.create(
+      ID.unique(),
+      user.name,
+      user.email,
+      user.password
+    );
+
+    if (!createdAccount) {
+      throw new Error("An Error Occurred while creating an Account!");
+    }
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: createdAccount.$id,
+      name: createdAccount.name,
+      email: createdAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// SAVE THE USER TO DATABASE
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: string;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteIds.databaseId,
+      appwriteIds.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// SIGN IN FUNCTION
+export async function signInUser(user: { email: string; password: string }) {
+  try {
+    const session = await account.createEmailPasswordSession(
+      user.email,
+      user.password
+    );
+
+    return session;
+  } catch (error) {}
+}
