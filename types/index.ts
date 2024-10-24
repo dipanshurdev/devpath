0ce35f029e2a432efd2844ff5da55faabcc8960e
Dipanshu Rawat <dv451197@gmail.com>
2024-10-24 21:56:40 +0530

    add: some types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,52 @@
+export type INavLink = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tag?: string;
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tag?: string;
+};
+
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+
+export type INewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
+
 export type AppwriteIds = {
   projectId: string | undefined;
   databaseId: string | undefined;
