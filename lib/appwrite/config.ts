b7761b6e6f7a8f044f87a832b15c3a77953529f9
Dipanshu Rawat <dv451197@gmail.com>
2024-10-24 21:56:24 +0530

    ref: env

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,15 +1,18 @@
+"use client";
+
 import { Client, Account, Databases, Avatars } from "appwrite";
-export const client = new Client();
 
 export const appwriteIds = {
-  projectId: process.env.NEXT_APP_APPWRITE_PROJECT_ID as string,
-  databaseId: process.env.NEXT_APP_APPWRITE_DATABASE_ID as string,
-  projectUrl: process.env.NEXT_APP_APPWRITE_URL as string,
-  userCollectionId: process.env.NEXT_APP_APPWRITE_USERS_COLLECTION_ID as string,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string,
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+  projectUrl: process.env.NEXT_PUBLIC_APPWRITE_URL as string,
+  userCollectionId: process.env
+    .NEXT_PUBLIC_APPWRITE_USERS_COLLECTION_ID as string,
 };
 
-client.setEndpoint(appwriteIds.projectUrl);
-client.setProject(appwriteIds.projectId);
+export const client = new Client();
+
+client.setProject(appwriteIds.projectId).setEndpoint(appwriteIds.projectUrl);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
