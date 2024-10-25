80a5bbfd7470304ec478aae99f83ce7f93bbb14a
Dipanshu Rawat <dv451197@gmail.com>
2024-10-25 09:37:31 +0530

    add: signOut function

--- lib/appwrite/api.ts
+++ lib/appwrite/api.ts
@@ -1,26 +1,5 @@
 "use client";
 
-// /my-nextjs-app
-// ├── /public                 # Static files like images and fonts
-// ├── /src
-// │   ├── /components         # Reusable UI components
-// │   ├── /pages              # Next.js pages (auto-routed)
-// │   │   ├── /api            # API routes for Next.js
-// │   │   ├── _app.js         # Custom App component
-// │   │   └── index.js        # Home page
-// │   ├── /lib                # Utility functions and libraries
-// │   │   └── appwrite.js     # Appwrite client initialization
-// │   ├── /hooks              # Custom React hooks (e.g., useAuth, useFetch)
-// │   ├── /context            # React context providers (e.g., AuthContext)
-// │   ├── /styles             # Global styles and component-specific styles
-// │   └── /types              # TypeScript definitions (if using TypeScript)
-// ├── /config                 # Configuration files (e.g., environment variables)
-// │   └── appwriteConfig.js   # Appwrite configuration (e.g., endpoints, project ID)
-// ├── /tests                  # Unit and integration tests
-// ├── .env                    # Environment variables
-// ├── package.json            # Project metadata and dependencies
-// └── next.config.js          # Next.js configuration
-
 import { INewUser } from "@/types";
 import { account, appwriteIds, avatars, databases } from "./config";
 import { ID, Query } from "appwrite";
@@ -122,5 +101,16 @@ export async function getCurrentUser() {
   } catch (error) {
     console.log(error);
     return null;
+  }
+}
+
+// ============================== SIGN OUT
+export async function signOutAccount() {
+  try {
+    const session = await account.deleteSession("current");
+
+    return session;
+  } catch (error) {
+    console.log(error);
   }
 }
