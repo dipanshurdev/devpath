1be1a62368faaea091365bbc8e8aee9885b1db5e
Dipanshu Rawat <dv451197@gmail.com>
2025-01-25 22:41:52 +0530

    chore: removed comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-// import Footer from "@/components/Footer";
 import CTA from "@/components/CTA";
 import FAQ from "@/components/FAQs";
 import Features from "@/components/Features";
 import Hero from "@/components/hero/Hero";
-// import Navbar from "@/components/navbar/Navbar";
-// import OpenSourceSection from "@/components/OSS";
-// import Roadmaps from "@/components/roadmaps/Roadmaps";
-// import { Roles, Lang } from "@/lib/randomStack";
 
 export default function Home() {
   return (
@@ -15,9 +10,6 @@ export default function Home() {
       <Features />
       <FAQ />
       <CTA />
-
-      {/* <Roadmaps title="Skill Based" inBuilding /> */}
-      {/* <OpenSourceSection /> */}
     </div>
   );
 }
