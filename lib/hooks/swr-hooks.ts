1f7607f0d08052b9b2b8ca6309ca2f89c78fa66e
Dipanshu Rawat <dv451197@gmail.com>
2025-04-04 01:01:05 +0530

    add: SWR for querry and chaching

---
+++ lib/hooks/swr-hooks.ts
@@ -0,0 +1,49 @@
+import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
+import {
+  getRoadmaps,
+  getRoadmapById,
+  getCurrentUser,
+  getNodes,
+  likeRoadmap,
+  saveRoadmap,
+} from "../appwrite/api";
+
+export function useGetRoadmaps() {
+  return useSWR("roadmaps", getRoadmaps);
+}
+
+export function useGetRoadmap(roadmapId: string) {
+  return useSWR(roadmapId ? `roadmap-${roadmapId}` : null, () =>
+    getRoadmapById(roadmapId)
+  );
+}
+
+export function useGetCurrentUser() {
+  return useSWR("current-user", getCurrentUser);
+}
+
+export function useNodes(nodeId: string) {
+  return useSWR(nodeId ? `nodes-${nodeId}` : null, () => getNodes(nodeId));
+}
+
+export function useLikeRoadmap() {
+  return useSWRMutation(
+    "roadmaps", // Key for SWR to trigger revalidation
+    async (_, { arg }) => {
+      const { roadmapId, updatedLikes } = arg;
+      return await likeRoadmap(roadmapId, updatedLikes);
+    }
+  );
+}
+
+// Hook for handling save functionality
+export function useSaveRoadmap() {
+  return useSWRMutation(
+    "saved-roadmaps", // Key for SWR to trigger revalidation
+    async (_, { arg }) => {
+      const { roadmapId, userId } = arg;
+      return await saveRoadmap(roadmapId, userId);
+    }
+  );
+}
