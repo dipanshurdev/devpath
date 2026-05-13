01eb04340b65db99407c7d1f2faf69e11ac173b0
Dipanshu Rawat <dv451197@gmail.com>
2026-05-13 10:05:28 +0530

    chore: cleanup

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// export const getDifficultyColor = (difficulty: string) => {
+//   switch (difficulty) {
+//     case "Beginner":
+//       return "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300";
+//     case "Intermediate":
+//       return "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300";
+//     case "Advanced":
+//       return "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300";
+//     default:
+//       return "bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300";
+//   }
+// };
