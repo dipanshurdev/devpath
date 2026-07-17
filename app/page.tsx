f5f59b888c88deeb42b05878588a29ebb3f40d64
Dipanshu Rawat <dv451197@gmail.com>
2026-07-18 00:44:14 +0530

    feat: add TrendingRoadmaps component to the homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import FAQ from "@/components/FAQs";
 import Features from "@/components/Features";
 import Hero from "@/components/hero/Hero";
 import LandingFooter from "@/components/landing-footer";
+import TrendingRoadmaps from "@/components/roadmaps/TrendingRoadmaps";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <TrendingRoadmaps />
       <Features />
       <FAQ />
       <CTA />
